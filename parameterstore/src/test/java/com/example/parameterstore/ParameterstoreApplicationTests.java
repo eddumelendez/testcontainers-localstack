8b76647ca33b64cbcde1badb3ea8ad61a9489974
Eddú Meléndez <eddu.melendez@gmail.com>
2023-11-03 23:14:36 -0600

    Use ConfigDataApplicationContextInitializer in parameterstore example

--- parameterstore/src/test/java/com/example/parameterstore/ParameterstoreApplicationTests.java
+++ parameterstore/src/test/java/com/example/parameterstore/ParameterstoreApplicationTests.java
@@ -1,12 +1,15 @@
 package com.example.parameterstore;
 
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -18,30 +21,31 @@ import static org.hamcrest.Matchers.equalTo;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT,
 		properties = { "spring.cloud.aws.credentials.access-key=noop", "spring.cloud.aws.credentials.secret-key=noop",
-				"spring.cloud.aws.region.static=us-east-1", "spring.config.import=aws-parameterstore:/spring/config/" })
+				"spring.cloud.aws.region.static=us-east-1" })
+@ContextConfiguration(initializers = ConfigDataApplicationContextInitializer.class)
 @Testcontainers
 class ParameterstoreApplicationTests {
+
+	@LocalServerPort
+	private int localPort;
 
 	@Container
 	private static LocalStackContainer localstack = new LocalStackContainer(
 			DockerImageName.parse("localstack/localstack:2.3.2"));
 
-	@LocalServerPort
-	private int localPort;
+	@DynamicPropertySource
+	static void properties(DynamicPropertyRegistry registry) {
+		registry.add("spring.cloud.aws.parameterstore.endpoint", () -> localstack.getEndpoint().toString());
+		registry.add("spring.cloud.aws.parameterstore.region", localstack::getRegion);
+		registry.add("spring.cloud.aws.endpoint", () -> localstack.getEndpoint().toString());
+		registry.add("spring.cloud.aws.region.static", localstack::getRegion);
+		registry.add("spring.config.import", () -> "aws-parameterstore:/spring/config/");
+	}
 
 	@BeforeAll
 	static void beforeAll() throws IOException, InterruptedException {
-		System.setProperty("spring.cloud.aws.parameterstore.endpoint", localstack.getEndpoint().toString());
-		System.setProperty("spring.cloud.aws.parameterstore.region", localstack.getRegion());
-
 		localstack.execInContainer("awslocal", "ssm", "put-parameter", "--name", "/spring/config/text", "--value",
 				"Hello World", "--type", "String", "--region", localstack.getRegion());
-	}
-
-	@AfterAll
-	static void afterAll() {
-		System.clearProperty("spring.cloud.aws.parameterstore.endpoint");
-		System.clearProperty("spring.cloud.aws.parameterstore.region");
 	}
 
 	@Test
