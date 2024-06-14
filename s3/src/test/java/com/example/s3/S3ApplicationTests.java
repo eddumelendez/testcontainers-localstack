cfde878754a2e07401653df6bd35f943005a8cb4
Eddú Meléndez <eddu.melendez@gmail.com>
2024-06-14 17:24:39 +0200

    Configure bucket notifications

--- s3/src/test/java/com/example/s3/S3ApplicationTests.java
+++ s3/src/test/java/com/example/s3/S3ApplicationTests.java
@@ -1,20 +1,30 @@
 package com.example.s3;
 
 import io.awspring.cloud.s3.S3Template;
+import io.awspring.cloud.sqs.annotation.SqsListener;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.eventnotifications.s3.model.S3EventNotification;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
+import software.amazon.awssdk.services.sqs.SqsClient;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,10 +43,36 @@ class S3ApplicationTests {
 	@Autowired
 	private S3Client s3Client;
 
+	@Autowired
+	private TestListener testListener;
+
+	private static final String POLICY = """
+						{
+			               "QueueConfigurations": [
+			                   {
+			                       "QueueArn": "%s",
+			                       "Events": ["s3:ObjectCreated:*"]
+			                   }
+			               ]
+			           }
+			""";
+
 	@BeforeAll
 	static void beforeAll() throws IOException, InterruptedException {
 		localstack.execInContainer("awslocal", "s3api", "create-bucket", "--bucket", "conferences", "--region",
 				localstack.getRegion());
+		var sqsClient = SqsClient.builder()
+			.region(Region.of(localstack.getRegion()))
+			.credentialsProvider(StaticCredentialsProvider
+				.create(AwsBasicCredentials.create(localstack.getAccessKey(), localstack.getSecretKey())))
+			.endpointOverride(localstack.getEndpoint())
+			.build();
+		var queue = sqsClient.createQueue(builder -> builder.queueName("s3-event-notification-queue"));
+		var queueAttributes = sqsClient
+			.getQueueAttributes(builder -> builder.queueUrl(queue.queueUrl()).attributeNamesWithStrings("QueueArn"));
+		var queueArn = queueAttributes.attributesAsStrings().get("QueueArn");
+		localstack.execInContainer("awslocal", "s3api", "put-bucket-notification-configuration", "--bucket",
+				"conferences", "--notification-configuration", String.format(POLICY, queueArn));
 	}
 
 	@Test
@@ -55,6 +91,31 @@ class S3ApplicationTests {
 		ListObjectsV2Response listTalksObjectsV2Response = this.s3Client
 			.listObjectsV2(ListObjectsV2Request.builder().bucket("talks").build());
 		assertThat(listTalksObjectsV2Response.contents()).hasSize(1);
+		assertThat(testListener.messages).hasSize(2);
+		var s3EventNotification = S3EventNotification.fromJson(testListener.messages.get(1));
+		assertThat(s3EventNotification.getRecords().getFirst().getEventSource()).isEqualTo("aws:s3");
+		assertThat(s3EventNotification.getRecords().getFirst().getEventName()).isEqualTo("ObjectCreated:Put");
+	}
+
+	@TestConfiguration(proxyBeanMethods = false)
+	static class TestConfig {
+
+		@Bean
+		TestListener testListener() {
+			return new TestListener();
+		}
+
+	}
+
+	static class TestListener {
+
+		private final List<String> messages = new ArrayList<>();
+
+		@SqsListener("s3-event-notification-queue")
+		void listen(String message) {
+			this.messages.add(message);
+		}
+
 	}
 
 }
