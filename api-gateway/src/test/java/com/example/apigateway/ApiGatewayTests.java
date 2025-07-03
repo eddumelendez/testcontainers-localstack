101149d5f8e9f220fe9da746351ce1c5211244cc
Eddú Meléndez <eddu.melendez@gmail.com>
2025-07-03 09:21:03 -0600

    Update localstack version to 4.6.0

--- api-gateway/src/test/java/com/example/apigateway/ApiGatewayTests.java
+++ api-gateway/src/test/java/com/example/apigateway/ApiGatewayTests.java
@@ -31,7 +31,7 @@ class ApiGatewayTests {
 
 	@Container
 	private static final LocalStackContainer localstack = new LocalStackContainer(
-			DockerImageName.parse("localstack/localstack:4.5.0"));
+			DockerImageName.parse("localstack/localstack:4.6.0"));
 
 	@Test
 	void test() {
--- ses/src/test/java/com/example/ses/SesApplicationTests.java
+++ ses/src/test/java/com/example/ses/SesApplicationTests.java
@@ -27,7 +27,7 @@ class SesApplicationTests {
 	@Container
 	@ServiceConnection
 	static LocalStackContainer localStackContainer = new LocalStackContainer(
-			DockerImageName.parse("localstack/localstack:4.5.0"));
+			DockerImageName.parse("localstack/localstack:4.6.0"));
 
 	@Autowired
 	private MailSender mailSender;
